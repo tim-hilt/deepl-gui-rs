2905a0b684fab32503da01ed28ef2068cb63f982
Tim Hilt <thilt@in-technology.eu>
2023-02-19 01:21:19 +0100

    main: get rid of serde-json, make req type-safe

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,35 @@
 use reqwest::header::AUTHORIZATION;
-use serde::Deserialize;
-use serde_json::json;
+use serde::{Deserialize, Serialize};
 use std::env;
 
+#[derive(Serialize)]
+struct Req {
+    target_lang: &'static str,
+    source_lang: &'static str,
+    text: [&'static str; 1],
+}
+
 #[derive(Deserialize)]
-struct Res {
+struct T {
     text: String,
 }
 
 #[derive(Deserialize)]
-struct Trans {
-    translations: Vec<Res>,
+struct Res {
+    translations: [T; 1],
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let api_key = env::var("DEEPL_API_KEY").expect("DEEPL_API_KEY not set");
+    let api_key: String = env::var("DEEPL_API_KEY").expect("DEEPL_API_KEY not set");
 
-    // TODO: Strongly type this!
-    let body = json!({
-        "text": ["Hallo, Welt!"],
-        "target_lang": "EN",
-        "source_lang": "DE"
-    });
+    let body = Req {
+        source_lang: "DE",
+        target_lang: "EN",
+        text: ["Hallo, Welt!"],
+    };
 
-    let resp: Trans = reqwest::Client::new()
+    let res: Res = reqwest::Client::new()
         .post("https://api-free.deepl.com/v2/translate")
         .json(&body)
         .header(AUTHORIZATION, format!("DeepL-Auth-Key {}", api_key))
@@ -33,11 +38,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json()
         .await?;
 
-    if resp.translations.len() == 1 {
-        println!("{}", resp.translations[0].text);
-    } else {
-        panic!("there should be a single translation");
-    }
+    println!("{}", res.translations[0].text);
 
     Ok(())
 }
