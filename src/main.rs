453bc0c00ee7c90b56df3a7bd3be887a2059f587
Tim Hilt <thilt@in-technology.eu>
2023-02-26 21:15:18 +0100

    main: Make egui and reqwest play nice together

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,199 @@
-use reqwest::blocking::Client;
+// TODO: What are these actually doing?
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+
+use eframe::egui;
 use reqwest::header::AUTHORIZATION;
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::env;
+use std::fmt;
+use std::sync::mpsc::{Receiver, Sender};
+use std::time::Duration;
+use strum::{EnumIter, IntoEnumIterator};
+use tokio::runtime::Runtime;
 
-#[derive(Serialize)]
+fn main() {
+    let rt = Runtime::new().expect("Unable to create Runtime");
+
+    // Enter the runtime so that `tokio::spawn` is available immediately.
+    let _enter = rt.enter();
+
+    // Execute the runtime in its own thread.
+    // The future doesn't have to do anything. In this example, it just sleeps forever.
+    std::thread::spawn(move || {
+        rt.block_on(async {
+            loop {
+                tokio::time::sleep(Duration::from_secs(3600)).await;
+            }
+        })
+    });
+
+    // Run the GUI in the main thread.
+    eframe::run_native(
+        "DeepL GUI",
+        eframe::NativeOptions::default(),
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
+}
+#[derive(Debug, PartialEq, Clone, Copy, Serialize, EnumIter)]
+enum SourceLang {
+    BG,
+    CS,
+    DA,
+    DE,
+    EL,
+    EN,
+    ES,
+    ET,
+    FI,
+    FR,
+    HU,
+    ID,
+    IT,
+    JA,
+    KO,
+    LT,
+    LV,
+    NB,
+    NL,
+    PL,
+    PT,
+    RO,
+    RU,
+    SK,
+    SL,
+    SV,
+    TR,
+    UK,
+    ZH,
+}
+
+impl fmt::Display for SourceLang {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy, Serialize, EnumIter)]
+enum TargetLang {
+    BG,
+    CS,
+    DA,
+    DE,
+    EL,
+    EN,
+    ENGB,
+    ENUS, // TODO: Render as EN-US? Otherwise not usable
+    ES,
+    ET,
+    FI,
+    FR,
+    HU,
+    ID,
+    IT,
+    JA,
+    KO,
+    LT,
+    LV,
+    NB,
+    NL,
+    PL,
+    PT,
+    PTBR,
+    PTPT,
+    RO,
+    RU,
+    SK,
+    SL,
+    SV,
+    TR,
+    UK,
+    ZH,
+}
+
+impl fmt::Display for TargetLang {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+#[derive(Clone, Serialize)]
 struct Req {
-    target_lang: String,
-    source_lang: String,
+    source_lang: SourceLang,
+    target_lang: TargetLang,
     text: [String; 1],
+}
+
+struct MyApp {
+    // Sender/Receiver for async notifications.
+    tx: Sender<String>,
+    rx: Receiver<String>,
+
+    req: Req,
+    translation: String,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        let (tx, rx) = std::sync::mpsc::channel();
+
+        Self {
+            tx,
+            rx,
+            req: Req {
+                source_lang: SourceLang::DE,
+                target_lang: TargetLang::EN,
+                text: ["".to_owned()],
+            },
+            translation: "".to_owned(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        if let Ok(translation) = self.rx.try_recv() {
+            self.translation = translation
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            egui::ComboBox::from_label("Source Language")
+                .selected_text(self.req.source_lang.to_string())
+                .show_ui(ui, |ui| {
+                    for source_lang in SourceLang::iter() {
+                        ui.selectable_value(
+                            &mut self.req.source_lang,
+                            source_lang,
+                            source_lang.to_string(),
+                        );
+                    }
+                });
+
+            egui::ComboBox::from_label("Target Language")
+                .selected_text(self.req.target_lang.to_string())
+                .show_ui(ui, |ui| {
+                    for target_lang in TargetLang::iter() {
+                        ui.selectable_value(
+                            &mut self.req.target_lang,
+                            target_lang,
+                            target_lang.to_string(),
+                        );
+                    }
+                });
+
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Query: ");
+                ui.text_edit_singleline(&mut self.req.text[0])
+                    .labelled_by(name_label.id); // TODO: Call API debounced
+                                                 // TODO: Focus text-field on start
+            });
+            if ui.button("Send Request").clicked() {
+                send_req(self.req.clone(), self.tx.clone(), ctx.clone());
+            }
+            ui.label(self.translation.clone())
+        });
+    }
 }
 
 #[derive(Deserialize)]
@@ -20,31 +206,22 @@ struct Res {
     translations: [T; 1],
 }
 
-fn make_request() -> Result<String, Box<dyn std::error::Error>> {
+fn send_req(req: Req, tx: Sender<String>, ctx: egui::Context) {
     let api_key: String = env::var("DEEPL_API_KEY").expect("DEEPL_API_KEY not set");
+    tokio::spawn(async move {
+        let body = Client::default()
+            .post("https://api-free.deepl.com/v2/translate")
+            .json(&req)
+            .header(AUTHORIZATION, format!("DeepL-Auth-Key {}", api_key))
+            .send()
+            .await
+            .expect("Unable to send request")
+            .json::<Res>()
+            .await
+            .expect("Unable to parse response");
 
-    let client = Client::new();
-
-    let body = Req {
-        source_lang: "DE".to_string(),
-        target_lang: "EN".to_string(),
-        text: ["Hallo, Welt!".to_string()],
-    };
-
-    let res = client
-        .post("https://api-free.deepl.com/v2/translate")
-        .json(&body)
-        .header(AUTHORIZATION, format!("DeepL-Auth-Key {}", api_key))
-        .send()?
-        .json::<Res>()?;
-
-    let translation = res.translations.into_iter().nth(0).unwrap();
-    Ok(translation.text)
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let translation = make_request()?;
-
-    println!("{}", translation);
-    Ok(())
+        let translation = body.translations.into_iter().nth(0).unwrap();
+        let _ = tx.send(translation.text);
+        ctx.request_repaint();
+    });
 }
